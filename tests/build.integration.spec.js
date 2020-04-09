df9a675b7af58450c347c62036580133ea5f03fc
vlad <vlad.blindu@gmail.com>
2020-04-09 10:03:08 +0300

    - specific integretion tests added

--- tests/build.integration.spec.js
+++ tests/build.integration.spec.js
@@ -7,7 +7,7 @@ const {DEFINITIONS_DIR} = require('../__constants')
 
 describe('INTEGRATION TEST - main build', () => {
 
-    const errorClasses = registerErrorClasses()
+    const {errorClasses, normalize} = registerErrorClasses()
 
     it('Should load all defined error classes', () => {
 
@@ -29,5 +29,42 @@ describe('INTEGRATION TEST - main build', () => {
                 }
             )
         }
+    })
+
+    it('Should normalize a node error', () => {
+
+        const err = {
+            code: 'ECONNRESET',
+            message: 'Connection reset by peer.'
+        }
+        const eErr = normalize(err)
+        expect(eErr.className).to.equal('NodeError')
+        expect(eErr.message).to.equal('Connection reset by peer.')
+        expect(eErr.errorName).to.equal('CONNECTION_RESET_ERROR')
+    })
+
+    it('Should normalize a http error', () => {
+
+        const err = {
+            status: 403
+        }
+        const eErr = normalize(err)
+        expect(eErr.className).to.equal('HttpError')
+        expect(eErr.message).to.equal('Invalid credentials for requested resource')
+        expect(eErr.errorName).to.equal('NO_PERMISSION_ERROR')
+    })
+
+    it('Should normalize a auth/firebase error', () => {
+
+        const err = {
+            errorInfo: {
+                code: "auth/id-token-expired",
+                message: 'Id token has expired'
+            }
+        }
+        const eErr = normalize(err)
+        expect(eErr.className).to.equal('AuthError')
+        expect(eErr.message).to.equal('Id token has expired')
+        expect(eErr.code).to.equal('TKNEXPERR')
     })
 })
